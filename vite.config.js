e3a5d3e58b898855bad8f991a531838b9f3e425a
Joe Hawes <morehawes@gmail.com>
2026-03-19 18:36:12 -0700

    Refactor project structure to standard Vite layout

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from "vite";
-import path from "path";
 
 export default defineConfig({
   build: {
     outDir: "dist",
-    emptyOutDir: false,
-    rollupOptions: {
-      input: {
-        site: path.resolve(__dirname, "src/site.js"),
-      },
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`,
-      },
-    },
+    emptyOutDir: true,
   },
 });
